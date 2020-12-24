2de65f96460a36b4374b0b9549741c949f0ff68d
Andrea Salvatori <andrea.salvatori92@gmail.com>
2020-12-24 18:48:59 +0100

    Fix invalid parts tests

--- src/tests/tests_part_invalid.py
+++ src/tests/tests_part_invalid.py
@@ -6,36 +6,10 @@ from tests.statics.invalid_ranges import invalid_ranges
 
 
 class PartTestInvalid(unittest.TestCase):
-    @unittest.expectedFailure
+
     def test_from_string_invalid_0(self):
-        part = Part(units[invalid_ranges[0]['unit']], {})
-        self.assertRaises(ValueError, part.from_string(invalid_ranges[0]['input']))
-
-    @unittest.expectedFailure
-    def test_from_string_invalid_1(self):
-        part = Part(units[invalid_ranges[1]['unit']], {})
-        self.assertRaises(ValueError, part.from_string(invalid_ranges[1]['input']))
-
-    @unittest.expectedFailure
-    def test_from_string_invalid_2(self):
-        part = Part(units[invalid_ranges[2]['unit']], {})
-        self.assertRaises(ValueError, part.from_string(invalid_ranges[2]['input']))
-
-    @unittest.expectedFailure
-    def test_from_string_invalid_3(self):
-        part = Part(units[invalid_ranges[3]['unit']], {})
-        self.assertRaises(ValueError, part.from_string(invalid_ranges[3]['input']))
-
-    @unittest.expectedFailure
-    def test_from_string_invalid_4(self):
-        part = Part(units[invalid_ranges[4]['unit']], {})
-        self.assertRaises(ValueError, part.from_string(invalid_ranges[4]['input']))
-
-    @unittest.expectedFailure
-    def test_from_string_invalid_5(self):
-        part = Part(units[invalid_ranges[5]['unit']], {})
-        self.assertRaises(ValueError, part.from_string(invalid_ranges[5]['input']))
-
-
-if __name__ == "__main__":
-    unittest.main()
+        for invalid_range in invalid_ranges:
+            with self.subTest(range=invalid_range):
+                part = Part(units[invalid_range['unit']], {})
+                with self.assertRaises(ValueError, msg=invalid_range['error']):
+                    part.from_string(invalid_range['input'])
