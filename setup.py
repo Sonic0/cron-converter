5a1022f3327a7f5c303962935d0282544247a0b1
Andrea Salvatori - IU6FZL <andrea.salvatori92@gmail.com>
2023-04-09 23:28:23 +0200

    Bump to version 1.0.2 (#8)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cron-converter',
-    version='1.0.1',
+    version='1.0.2',
     license='MIT License',
     description='Cron string parser and scheduler for Python',
     long_description=open('README.md').read(),
