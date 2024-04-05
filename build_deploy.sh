c14b2cd8d0c540cd0496e0498e8e96f140835683
Andrea Salvatori - IU6FZL <16443598+Sonic0@users.noreply.github.com>
2024-04-05 19:31:29 +0200

    Update packaging tool configuration (#12)

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
+#================================================================
+# DESCRIPTION
+# This script builds the Python library and uploads the package either to testPyPi or PyPi, depending on the first argument provided.
+# The argument can be '--local' for local testing, '--test' for uploading to testPyPi, or left empty for uploading to PyPi.
+#================================================================
 
-TEST_URL="https://test.pypi.org/legacy/"
+print_msg() {
+  local GRE='\e[1;32m'
+  local NC='\e[0m'
+  echo -e "${GRE}$1${NC}"
+  return 0
+}
 
 pip3 install -r requirements/release.txt
 
-[ -d "dist" ] && rm -r dist && echo -e '\e[38;5;219m./dist dir deleted\e[0m'
-python3 setup.py sdist bdist_wheel
-[[ "$1" = "--local" ]] && echo -e '\e[38;5;219mtar.gz created in ./dist, install it locally via pip\e[0m' && exit 0
+[ -d "dist" ] && rm -r dist && print_msg './dist dir deleted'
+python3 -m build
+[[ "$1" = "--local" ]] && print_msg 'tar.gz created in ./dist, install it locally via pip' && exit 0
 
 if twine check dist/*; then
   if [ "$1" = "--test" ] ; then
-    twine upload --repository-url ${TEST_URL} dist/*
+    twine upload --repository testpypi dist/*
+    print_msg "Use this command for local testing:
+      python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps cron-converter"
   else
     twine upload dist/*
   fi
